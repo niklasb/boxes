e482fcfd62f7628fabe9f6dbcd2ef960c39c7f89
Samuel Groß <dev@samuel-gross.de>
2014-11-06 21:45:15 +0100

    Minor script improvements

--- scripts/common/zsh.sh
+++ scripts/common/zsh.sh
@@ -6,4 +6,4 @@ git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh &> /dev/null
 curl -s https://gist.githubusercontent.com/saelo/7614863612553a4647ae/raw/.zshrc > ~/.zshrc
 
 # needs sudo to work passwordless
-sudo chsh -s /usr/bin/zsh sam
+sudo chsh -s /usr/bin/zsh $(whoami)
--- scripts/ubuntu/websrv.sh
+++ scripts/ubuntu/websrv.sh
@@ -6,4 +6,5 @@
 echo mysql-server mysql-server/root_password password insecure | sudo debconf-set-selections
 echo mysql-server mysql-server/root_password_again password insecure | sudo debconf-set-selections
 
+apt-get update
 apt-get -y install apache2 php5 mysql-server php5-mysql
