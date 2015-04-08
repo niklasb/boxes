37a0c053d9960f386e23807c7d09e4befdd66041
Samuel Groß <dev@samuel-gross.de>
2015-04-08 11:42:46 +0200

    Updated Ubuntu 14.04 packer configurations

--- packer/scripts/debian/packages.sh
+++ packer/scripts/debian/packages.sh
@@ -9,4 +9,4 @@ echo "Installing VirtualBox guest additions..."
 apt-get -y install virtualbox-guest-dkms
 
 echo "Installing miscellaneous packages..."
-apt-get -y install htop vim git curl zsh build-essential tree cmake
+apt-get -y install htop vim git curl zsh build-essential tree cmake python-dev
